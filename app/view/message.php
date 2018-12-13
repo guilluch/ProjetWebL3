b1199ebb0c89727cc084641f62a8cda3715fd50d
uapv-lluch <guillaume.lluch@alumni.univ-avignon.fr>
2018-12-13 10:43:01 +0100

    Create message.php

---
+++ app/view/message.php
@@ -0,0 +1,17 @@
+<div class="post card slight-rounded hoverable">
+    <div class="card-header">
+        <span>Utilisateur</span>
+    </div>
+    <div class="card-body">
+        <p>
+            Du texte
+        </p>
+    </div>
+    <?php
+    echo '<div class="card-footer vote-btn-container">
+        <button class="btn vote-btn full-rounded waves-effect"><i class="material-icons">thumb_up_alt</i></button>
+        <button class="btn vote-btn full-rounded waves-effect"><i class="material-icons">thumb_down_alt</i></button>
+    </div>'
+    ?>
+
+</div>
