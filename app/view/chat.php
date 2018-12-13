b078c395bdf1871a39f6e51434d5769ce9d898d9
Cassin Matthieu <uapv1704457@ecv-2w26-105.univ-avignon.fr>
2018-12-13 11:28:34 +0100

    Ajout du chat view

---
+++ app/view/chat.php
@@ -0,0 +1,13 @@
+<div class="post card slight-rounded hoverable">
+    <div class="card-header">
+        <span>Destinataire</span>
+    </div>
+    <div class="card-body">
+        <p>
+           
+        </p>
+    </div>
+    <div class="card-footer vote-btn-container">
+       <input type="text" name='Ecrivez un Message'>
+    </div>
+</div>
