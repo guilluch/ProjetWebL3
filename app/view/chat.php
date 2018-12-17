d2e8da53701ea9821bed4464f5960a6226bbdbc1
uapv-lluch <guillaume.lluch@alumni.univ-avignon.fr>
2018-12-17 18:02:19 +0100

    Update chat.php

--- app/view/chat.php
+++ app/view/chat.php
@@ -1,13 +1,17 @@
-<div class="post card slight-rounded hoverable">
+<div class="post card slight-rounded hoverable"  style="position:absolute; bottom:0">
     <div class="card-header">
-        <span>Destinataire</span>
+        <?php
+        $prenom = utilisateurTable::getUserById(10)['prenom'];
+        print($prenom);
+        ?> 
     </div>
     <div class="card-body">
+        <form method="post">
         <p>
-           
+
         </p>
-    </div>
+        </form>
     <div class="card-footer vote-btn-container">
-       <input type="text" name='Ecrivez un Message'>
+       <input type="text" type="submit" value="Ecrivez votre Message"> 
     </div>
 </div>
