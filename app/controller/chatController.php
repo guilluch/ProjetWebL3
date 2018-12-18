3842c57cbdcf0030153be6b798caeca7f2feaf17
Cassin Matthieu <uapv1704457@ecv-2w26-105.univ-avignon.fr>
2018-12-18 14:46:15 +0100

    Modif Chat Control

--- app/controller/chatController.php
+++ app/controller/chatController.php
@@ -1,3 +1,11 @@
 <?php
 
+function upload_image($request, $context){
+  
+    if(move_uploaded_file($_FILES['image']['tmp_name'],"images/".$_FILES['image']['name'])) {
+        $image = "https://pedago02a.univ-avignon.fr/~uapv1901496/images/".$_FILES['image']['name'];
+    }	            
+    else $image = "";
+    //aze errr
 
+}
